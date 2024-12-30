985ed42f359d6479e634953c6d439d23299af33e
Ela <micaelafrancisco13@outlook.com>
2024-12-30 14:45:53 +0800

    add sync of node_modules within container functionality

--- prisma/migrations/20241230064405_add_initial_migration/migration.sql
+++ prisma/migrations/20241230064405_add_initial_migration/migration.sql
@@ -1,4 +1,13 @@
 -- CreateTable
+CREATE TABLE "Item" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Product" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
